d570b577d53e23e4fed5808a8661afabfd1c397d
LFurman7 <79952153+LFurman7@users.noreply.github.com>
2021-08-30 07:03:44 -0400

    homeworks

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "22e222cc4a21bf50295acff3787c9bfa"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAjqHABStosAXRyTvQ49kY_JwYaZt6_RdA"
